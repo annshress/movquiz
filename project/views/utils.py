925d3daf633ca9102356fff1dddeaedccf1ce729
annshress <ann.shress@gmail.com>
2020-04-08 19:42:03 +0545

    polish

--- project/views/utils.py
+++ project/views/utils.py
@@ -1,13 +1,15 @@
 from functools import wraps
 
 from flask import redirect
+from flask.helpers import url_for
 from flask_login import current_user
 
 
 def logout_required(func):
+    """decorator: redirects to users.home for views requiring anonymous user"""
     @wraps(func)
     def decorated_view(*args, **kwargs):
         if current_user.is_authenticated:
-            return redirect('/')
+            return redirect(url_for('users.home'))
         return func(*args, **kwargs)
     return decorated_view
